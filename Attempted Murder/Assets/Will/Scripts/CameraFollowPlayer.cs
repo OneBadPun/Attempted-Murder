57beb5470522af5e64d5644a70e5b9e3c27fa348
William McLaughlin <williammclaughlin0@hotmail.com>
2020-10-23 19:31:38 +0100

    Added Basic Movement
    Added basic movement based on the direction of the camera

---
+++ Attempted Murder/Assets/Will/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+
+    Vector3 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameObject.FindGameObjectWithTag("Player"))
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+            target = player.transform.position;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            MoveCamera();
+        }
+
+    }
+    // Move camera based off player position
+    private void MoveCamera()
+    {
+        
+    }
+}
---
+++ Attempted Murder/Assets/Will/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // movement variables
+    public float moveSpeed = 4f;
+
+    Rigidbody playerBody;
+
+    Vector3 forward;
+    Vector3 right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+
+        forward = Camera.main.transform.forward;
+        forward.y = 0; // set to zero or else player will be pushed into the ground while moving forward
+        right = Camera.main.transform.right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveControls();
+        
+    }
+    // Move the player
+    void MoveControls()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerBody.AddForce(forward * moveSpeed);
+            Debug.Log(this.gameObject.transform.position);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            playerBody.AddForce(forward * -moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerBody.AddForce(right * -moveSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            playerBody.AddForce(right * moveSpeed);
+        }
+    }
+}
